27f7bc2d5c238725d7b7f16fd68ab02f31cc16b4
evin001 <e19a@yandex.ru>
2017-03-21 14:24:57 +0300

    i18n for english

---
+++ messages/en-US/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Submit' => 'Submit',
+    'Balance sheet' => 'Balance sheet',
+    'Report file' => 'Report file',
+    'Profit' => 'Profit',
+    'Commission' => 'Commission',
+    'Balance' => 'Balance',
+    'Report generation' => 'Report generation',
+];
